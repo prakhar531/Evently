0725a3edbbf7aad81658f129b0ac153e138c2e39
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-06 02:36:03 +0530

    implemented Related events in event details page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,11 +1,25 @@
-import { getEventById } from "@/lib/actions/event.action";
+import {
+  getEventById,
+  getRelatedEventsByCategory,
+} from "@/lib/actions/event.action";
 import { SearchParamProps } from "@/types";
 import React from "react";
 import Image from "next/image";
 import { formatDateTime } from "@/lib/utils";
+import Collection from "@/components/shared/Collection";
 
-const EventDetails = async ({ params: { id } }: SearchParamProps) => {
+const EventDetails = async ({
+  params: { id },
+  searchParams,
+}: SearchParamProps) => {
   const event = await getEventById(id);
+
+  const relatedEvents = await getRelatedEventsByCategory({
+    categoryId: event.category._id,
+    eventId: event._id,
+    page: searchParams.page as string,
+  });
+
   return (
     <>
       <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
@@ -91,7 +105,8 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
         </div>
       </section>
 
-      {/* <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+      {/* Related event means events with same category */}
+      <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
         <h2 className="h2-bold">Related Events</h2>
 
         <Collection
@@ -103,7 +118,7 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
           page={searchParams.page as string}
           totalPages={relatedEvents?.totalPages}
         />
-      </section> */}
+      </section>
     </>
   );
 };

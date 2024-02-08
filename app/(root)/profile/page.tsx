b2df92b1ed0e4b9da727634acc00bef787fcde04
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-08 12:50:52 +0530

    Designed Event Organized inside my profile

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,30 +1,65 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
+import { getEventsByUser } from "@/lib/actions/event.action";
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
-const ProfilePage = () => {
+const ProfilePage = async () => {
+  // Fetching user id for events
+
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
+  const organizedEvents = await getEventsByUser({ userId, page: 1 });
+
   return (
     <>
       {/* My tickets  */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My tickets</h3>
-          <Button asChild className="button hidden sm:flex">
+          <Button asChild size="lg" className="button hidden sm:flex">
             <Link href="/#events">Explore more Events</Link>
           </Button>
         </div>
       </section>
+
+      {/* <section className="wrapper my-8">
+        <Collection
+          data={events?.data}
+          emptyTitle="No event tickets purchased yet"
+          emptyStateSubtext="No worries-plenty of exciting events to explore!"
+          collectionType="My_Tickets"
+          limit={3}
+          page={1}
+          urlParamName="ordersPage"
+          totalPages={2}
+        />
+      </section> */}
 
       {/* event organized */}
 
       <section className="bg-primary-50 bg-dotted-pattern bg-cover py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">Event Organized</h3>
-          <Button asChild className="button hidden sm:flex">
-            <Link href="/#events">Create New Event</Link>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            <Link href="/events/create">Create New Event</Link>
           </Button>
         </div>
+      </section>
+
+      <section className="wrapper my-8">
+        <Collection
+          data={organizedEvents?.data}
+          emptyTitle="No events have been created yet"
+          emptyStateSubtext="Go create some now!"
+          collectionType="Events_Organized"
+          limit={6}
+          page={1}
+          urlParamName="ordersPage"
+          totalPages={2}
+        />
       </section>
     </>
   );
--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -42,10 +42,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         </div>
       )}
 
-      <Link
-        href={`/events/${event._id}`}
-        className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
-      >
+      <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
         {!hidePrice && (
           <div className="flex gap-2">
             <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
@@ -63,9 +60,11 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
           {formatDateTime(event.startDateTime).dateTime}
         </p>
 
-        <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
-          {event.title}
-        </p>
+        <Link href={`/events/${event._id}`}>
+          <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
+            {event.title}
+          </p>
+        </Link>
 
         <div className="flex-between w-full">
           <p className="p-medium-14 md:p-medium-16 text-grey-600">
@@ -85,7 +84,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
             </Link>
           )}
         </div>
-      </Link>
+      </div>
     </div>
   );
 };

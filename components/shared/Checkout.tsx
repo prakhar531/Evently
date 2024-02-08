e7b689214b8ec1570fba46c8ab1d13a579ffddb6
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-08 14:21:17 +0530

    Basic stripe setup- https://stripe.com/docs/checkout/quickstart

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,8 +1,26 @@
 import { IEvent } from "@/lib/mongodb/database/models/event.model";
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "../ui/button";
 
+import { loadStripe } from "@stripe/stripe-js";
+
+loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+
 const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
+    }
+  }, []);
+
   const onCheckout = async () => {
     console.log("Checkout");
   };

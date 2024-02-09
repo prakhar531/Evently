480faefeef8003a62a269d400b9119b9078ebe2e
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-09 18:40:33 +0530

    fix stripe error

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,17 +1,19 @@
 import stripe from "stripe";
 import { NextResponse } from "next/server";
+import { buffer } from "node:stream/consumers";
 import { createOrder } from "@/lib/actions/order.actions";
 
-export async function POST(request: Request) {
-  const body = await request.text();
+export async function POST(req: any) {
+  // const body = await request.text();
+  const rawBody = await buffer(req.body);
 
-  const sig = request.headers.get("stripe-signature") as string;
-  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+  const sig = req.headers.get("stripe-signature") as string;
+  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET! as string;
 
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+    event = stripe.webhooks.constructEvent(rawBody, sig, endpointSecret);
   } catch (err) {
     return NextResponse.json({ message: "Webhook error", error: err });
   }

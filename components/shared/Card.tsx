1bdf7496aafec232dcab36960bb90349cab8c558
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-02-11 19:53:06 +0530

    add orders details page

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -73,7 +73,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
           </p>
 
           {hasOrderLink && (
-            <Link href={"/orders?eventId=${event._id}"} className="flex gap-2">
+            <Link href={`/orders?eventId=${event._id}`} className="flex gap-2">
               <p className="text-primary-500">Order Details</p>
               <Image
                 src="/assets/icons/arrow.svg"
---
+++ components/shared/Search.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Search = ({ placeholder }: any) => {
+  return <div>Search</div>;
+};
+
+export default Search;
